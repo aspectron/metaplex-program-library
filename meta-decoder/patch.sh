55dbd89a7149f3d85cbdd0796d0fdcde77099153
Surinder Singh Matoo <surinder83singh@gmail.com>
2022-07-21 21:58:10 +0530

    patch script

--- meta-decoder/patch.sh
+++ meta-decoder/patch.sh
@@ -2,3 +2,5 @@ cp ../token-metadata/program/src/deser.rs ./src/
 cp ../token-metadata/program/src/utils.rs ./src/
 cp ../token-metadata/program/src/state.rs ./src/
 cp ../token-metadata/program/src/error.rs ./src/
+
+git apply ./decoder-p1.patch
